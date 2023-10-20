737d6da5090076af7e253508e1cb1cdbebb00510
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2023-10-20 16:03:45 +0200

    Delete test directory

--- test/src/inc/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require ("../src/inc/config.php");
-include ("../src/libs/helpers.php");
-include ("../src/libs/sanitization.php");
-include ("../src/libs/validation.php");
--- test/src/inc/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-if(isset($_SESSION['login']) && $_SESSION['login'] == true) {
-    header("location: ../src/index.php");
-}
-
-

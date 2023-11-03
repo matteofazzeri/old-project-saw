840440e5fbf5d748c2bdf88918f8e0a67e2ad768
matteofazzeri <matteofazzeri@gmail.com>
2023-11-03 11:48:20 +0100

    test

--- es11/src/inc/header.php
+++ es11/src/inc/header.php
@@ -1,22 +1,22 @@
 <header>
-    <div class="header">
-        <img src="..." alt="logo">
-        <nav>
-            <?php
-            if(isLogged()) {
-                echo '
-                    <span class="nav-item"><a href="../public/index.php">Home</a></span>
-                    <span class="nav-item"><a href="#">show profile</a></span>
-                    <span class="nav-item"><a href="../src/logout.php">logout</a></span>
-                ';
-            } else {
-                echo '
-                    <span class="nav-item"><a href="../public/index.php">Home</a></span>
-                    <span class="nav-item"><a href="../src/registration.php">signin</a></span>
-                    <span class="nav-item"><a href="../src/login.php">login</a></span>
-                ';
-            }
-            ?>
-        </nav>
-    </div>
+  <div class="header">
+    <img src="..." alt="logo">
+    <nav>
+      <?php
+      if (isLogged()) {
+        echo '
+          <span class="nav-item"><a href="../public/index.php">Home</a></span>
+          <span class="nav-item"><a href="#">show profile</a></span>
+          <span class="nav-item"><a href="../src/logout.php">logout</a></span>
+        ';
+      } else {
+        echo '
+          <span class="nav-item"><a href="../public/index.php">Home</a></span>
+          <span class="nav-item"><a href="../src/registration.php">signin</a></span>
+          <span class="nav-item"><a href="../src/login.php">login</a></span>
+        ';
+      }
+      ?>
+    </nav>
+  </div>
 </header>

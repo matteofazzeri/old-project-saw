5608b492ce6e97fcfce380092c687922376afe7d
matteofazzeri <matteofazzeri@gmail.com>
2023-11-13 19:11:35 +0100

    x

---
+++ react/react_test/src/App.js
@@ -0,0 +1,12 @@
+import './css/App.css';
+import Registration from './components/registration.js'
+
+function App() {
+  return (
+    <div>
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
